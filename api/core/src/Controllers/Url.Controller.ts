30b3b2069375457cdf6fbba1005557c376f2f623
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-02-24 14:39:49 -0300

    v1.10 - Fix Controller

--- api/core/src/Controllers/Url.Controller.ts
+++ api/core/src/Controllers/Url.Controller.ts
@@ -26,27 +26,23 @@ export class UrlController implements UrlControllerI {
     next: NextFunction
   ): Promise<void> {
     const { url }: { url: string } = req.body;
-    try {
-      const isUrlValid = await urlService.validateUrl(url);
+    const isUrlValid = await urlService.validateUrl(url);
 
-      if (!isUrlValid) {
-        // wrong input
-        res.status(422).json({ error: "invalid url" });
-        return next();
-      }
-
-      const dbResponse = await urlRepository.addUrl(url);
-
-      const response: ApiResponseI = {
-        original_url: url,
-        short_url: dbResponse.id,
-      };
-
-      // Code 200 - Ok
-      res.send(response);
-    } catch (error) {
-      res.status(500).json({ error: "Internal Error" });
+    if (!isUrlValid) {
+      // wrong input
+      res.status(422).json({ error: "invalid url" });
+      return next();
     }
+
+    const dbResponse = await urlRepository.addUrl(url);
+
+    const response: ApiResponseI = {
+      original_url: url,
+      short_url: dbResponse.id,
+    };
+
+    // Code 200 - Ok
+    res.json(response);
   }
 
   public async redirectToUrl(
@@ -55,25 +51,21 @@ export class UrlController implements UrlControllerI {
     next: NextFunction
   ): Promise<void> {
     const id = +req.params.id;
-    try {
-      if (isNaN(id)) {
-        // wrong input
-        res.sendStatus(422);
-        return next();
-      }
-
-      const dbResponse = await urlRepository.getUrlById(id);
-
-      if (!dbResponse?.id || !dbResponse?.url) {
-        // url not found
-        res.sendStatus(404);
-        return next();
-      }
-
-      // Code 302 - Redirect
-      res.redirect(dbResponse.url);
-    } catch (error) {
-      res.status(500).json({ error: "Internal Error" });
+    if (isNaN(id)) {
+      // wrong input
+      res.sendStatus(422);
+      return next();
     }
+
+    const dbResponse = await urlRepository.getUrlById(id);
+
+    if (!dbResponse?.id || !dbResponse?.url) {
+      // url not found
+      res.sendStatus(404);
+      return next();
+    }
+
+    // Code 302 - Redirect
+    res.redirect(dbResponse.url);
   }
 }
