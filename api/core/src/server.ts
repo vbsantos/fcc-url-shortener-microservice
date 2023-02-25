7319e6e39babf7b61cfe3617f6afc4173cf4e7c5
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-02-24 21:37:29 -0300

    debug

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -1,6 +1,6 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, { Express } from "express";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+import express, { Express, json } from "express";
 
 import { UrlController, UrlControllerI } from "./Controllers/Url.Controller";
 // import { UrlMiddleware, UrlMiddlewareI } from "./Middlerwares/Url.Middleware";
@@ -15,8 +15,8 @@ const urlController: UrlControllerI = new UrlController();
 const app: Express = express();
 
 // Middlewares
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors({ origin: "*" }));
+app.use(json());
 
 // Routes
 app.post("/shorturl", urlController.postShortUrl);
